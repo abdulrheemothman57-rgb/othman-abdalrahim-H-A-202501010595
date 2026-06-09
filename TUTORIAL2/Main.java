2b13573dd9e9ea7d215e30d02d9a0df982821415
abdulrheemothman57-rgb <abdulrheemothman57@gmail.com>
2026-06-09 07:40:52 +0000

    branch1

---
+++ TUTORIAL2/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Student s1 = new Student("Abdalrahim", 19, 3.4);
+        s1.displayInfo();
+        s1.study();
+        s1.displayInfo();
+    }
+}
