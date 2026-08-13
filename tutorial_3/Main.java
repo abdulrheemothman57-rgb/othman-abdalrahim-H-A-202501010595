b607e312c99f5446268eed10717a78d70325584c
abdulrheemothman57-rgb <abdulrheemothman57@gmail.com>
2026-08-13 16:41:56 +0000

    tutorial3

--- tutorial_3/Main.java
+++ tutorial_3/Main.java
@@ -3,7 +3,7 @@ package tutorial_3;
 public class Main {
     public static void main(String[] args) {
         
-        Person p1 = new Person("Ahmed", "1000");
+        Person p1 = new Person("Abdalrahim", "1000");
         Person p2 = new Student("Ali", "2000");
         Person p3 = new Lecturer("Dr Khalid", "3000");
 
