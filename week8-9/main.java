ffe366b05b1ce842208b30c292feca7bb5c7ef31
abdulrheemothman57-rgb <abdulrheemothman57@gmail.com>
2026-08-18 19:43:25 +0000

    tutorial_7

--- week8-9/main.java
+++ week8-9/main.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Main {
+public class main {
 
     public static void main(String[] args) {
         ArrayList<String> tasks = new ArrayList<>();
