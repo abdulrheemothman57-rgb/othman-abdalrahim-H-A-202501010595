a83c49edd02b693e8adca5fa03aaeb2f225094e9
abdulrheemothman57-rgb <abdulrheemothman57@gmail.com>
2026-08-18 19:33:46 +0000

    tutorial_2

--- tutorial_2/main.java
+++ tutorial_2/main.java
@@ -1,10 +1,10 @@
-public class main {
-    public static void main(String[] args) {
-        //coding starts here;
+public class main{
+    public static void main(String [] args){
+        Student s1 = new Student("",19, 3.6);
 
-        Student s1 = new Student("Abdalrahim",19,3.6);
         s1.displayInfo();
         s1.study();
         s1.takeExam();
+
     }
 }
